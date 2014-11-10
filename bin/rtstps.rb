75a6afd38d588a7358dcfdae54533235be339807
Jay Cable <jcable@sandy-dev.gina.alaska.edu>
2014-11-10 12:47:15 -0900

    more fixing action\!

--- bin/rtstps.rb
+++ bin/rtstps.rb
@@ -13,49 +13,48 @@ class RtstpsClamp <  ProcessingFramework::CommandLineHelper
   option ["-i", "--input"], "input", "The input file. ", :required => true
 
   def execute
-	   conf = ProcessingFramework::ConfigLoader.new(__FILE__)
+	conf = ProcessingFramework::ConfigLoader.new(__FILE__)
 
-	   output = "#{outdir}"
-	   outdir += "/" + basename if basename
-	   basename = File.basename(input) if !basename
+	output = "#{outdir}"
+	outdir += "/" + basename if basename
+	basename = File.basename(input) if !basename
 
-	   platform =  basename.split(".").first
+	platform =  basename.split(".").first
 
-		 #check platofrm
-     raise "Unknown/unconfigured platform #{platform}" if !conf["configs"][platform]
+	#check platofrm
+     	raise "Unknown/unconfigured platform #{platform}" if !conf["configs"][platform]
 
-	   working_dir = "#{tempdir}/#{basename}"
-     begin
+	working_dir = "#{tempdir}/#{basename}"
+     	begin
+		#make temp space
+		FileUtils.rm_r(working_dir) if (File.exists?(working_dir))
+		FileUtils.mkdir(working_dir)
+		sourcefile = ""
 
-		    #make temp space
-		    FileUtils.rm_r(working_dir) if (File.exists?(working_dir))
-		    FileUtils.mkdir(working_dir)
-
-        #make DATA (this is silly)
-        FileUtils.mkdir(working_dir + "/DATA/")
-		    FileUtils.cd(working_dir) do
-			       #This part is so we don't have to tweek the DATA part of the rtsps configs.
-             sourcefile = File.basename(input)
-             FileUtils.cp(input, sourcefile)
-             sourcefile = ProcessingFramework::CompressHelper.uncompress(sourcefile)
-			       puts("#{conf["rtsps_driver"]} #{conf["configs"][platform]}")
-				     #check output here
-        end
-        FileUtils.cd(working_dir + "/DATA/") do
-				     FileUtils.mkdir(output)  if (File.exists?(output))
-				     #copy output
-             Dir.glob("*").each do |x|
-               puts("INFO: Copying #{x} to #{output}")
-               FileUtils.cp(x, output)
-            end
-			  end
-		    FileUtils.rm_r(working_dir)
-	   rescue RuntimeError => e
-	      puts ("Error: #{e.to_s}")
-		    FileUtils.rm_r(working_dir) if (File.exists?(working_dir))
-		    exit(-1)
-	   end
-   end
+        	#make DATA (this is silly)
+        	FileUtils.mkdir(working_dir + "/DATA/")
+		FileUtils.cd(working_dir) do
+			#This part is so we don't have to tweek the DATA part of the rtsps configs.
+             		sourcefile = File.basename(input)
+             		FileUtils.cp(input, sourcefile)
+             		sourcefile = ProcessingFramework::CompressHelper.uncompress(sourcefile)
+        	end
+        	FileUtils.cd(working_dir + "/DATA/") do
+			ProcessingFramework::ShellOutHelper.run_shell("#{conf["rtsps_driver"]} #{conf["configs"][platform]} ../#{sourcefile}")
+	    		FileUtils.mkdir(output)  if (File.exists?(output))
+			#copy output
+            		Dir.glob("*").each do |x|
+               			puts("INFO: Copying #{x} to #{output}")
+               			FileUtils.cp(x, output)
+            		end
+		end
+		FileUtils.rm_r(working_dir)
+	rescue RuntimeError => e
+	      	puts ("Error: #{e.to_s}")
+		FileUtils.rm_r(working_dir) if (File.exists?(working_dir))
+		exit(-1)
+	end
+  end
 end
 
 RtstpsClamp.run
