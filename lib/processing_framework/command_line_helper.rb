d06b0d85262c924ed295b337026ad16efc4889fe
JC <jc@alaska.edu>
2014-11-11 17:41:46 -0900

    added output copy filtering.

--- lib/processing_framework/command_line_helper.rb
+++ lib/processing_framework/command_line_helper.rb
@@ -9,17 +9,16 @@ module ProcessingFramework
 		option ["-o", "--outdir"], "outdir", "The output directory. If --basename is included, it is appended to this.", :required => true 
 		option ["-t", "--tempdir"], "tempdir", "The temp directory, used for working space. A sub directory will made inside this named after the basename.",  :environment_variable => "PROCESSING_TEMPDIR", :required => true 
 
-
   		def execute
 			raise "CommandLineHelper should not be instatiated directly." 
     		end
 
 
 		#Perhaps not the best place for this - copies everything in the current directory to the directory at output
-		def copy_output( output)
+		def copy_output( output, save_glob="*")
 			output += "/" if output[-1] != "/"
 			FileUtils.mkdir(output)  if (!File.exists?(output))
-                        Dir.glob("*").each do |x|
+                        Dir.glob(save_glob).each do |x|
                                 puts("INFO: Copying #{x} to #{output}")
                                 FileUtils.cp(x, output)
                         end
