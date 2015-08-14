4eed8802091cbbe89511cc0abb6b3657a5b424ae
Scott Macfarlane <scott@gina.alaska.edu>
2015-08-13 16:41:35 -0800

    Refactor processing framework to support aapp requirements

--- lib/processing_framework/command_line_helper.rb
+++ lib/processing_framework/command_line_helper.rb
@@ -1,32 +1,42 @@
 module ProcessingFramework
   require 'clamp'
   require 'fileutils'
-  class CommandLineHelper  < Clamp::Command
-    # shared options
-    # @config = nil
+  require_relative 'shell_out_helper'
+  require_relative 'config_helper'
+
+  class CommandLineHelper < Clamp::Command
+    include ProcessingFramework::ShellOutHelper
+
+    option ['-c', '--config'], 'config', "Override default config file." do |c|
+      default_config File.realpath(c)
+    end
     option ['-b', '--basename'], 'basename', 'The basename of the data to be processed. For example npp.14300.1843 .  Appended to --outputdir if included.'
-    #		option ["-i", "--input"], "input", "The source directory. If --basename is included, it is appended to this.", :required => true
-    option ['-o', '--outdir'], 'outdir', 'The output directory. If --basename is included, it is appended to this.', required: true
     option ['-t', '--tempdir'], 'tempdir', 'The temp directory, used for working space. A sub directory will made inside this named after the basename.',  environment_variable: 'PROCESSING_TEMPDIR', required: true
+    # option ['-i', '--input'], 'input', 'The input file. ', required: true
+    # option ['-o', '--outdir'], 'outdir', 'The output directory. If --basename is included, it is appended to this.', required: true
+
 
     def execute
       fail 'CommandLineHelper should not be instatiated directly.'
     end
 
-    # Perhaps not the best place for this - copies everything in the current directory to the directory at output
-    def copy_output(output, save_glob = '*', copy_dirs = false)
-      # add trailing slash, if needed
-      output += '/' if output[-1] != '/'
-
-      FileUtils.mkdir_p(output)  unless (File.exist?(output))
-      Dir.glob(save_glob).each do |x|
-        if (File.file?(x) || copy_dirs)
-          puts("INFO: Copying #{x} to #{output}")
-          FileUtils.cp_r(x, output)
-        else
-          puts("INFO: Not a file, skipping #{x} to #{output}")
-        end
-      end
+    def conf
+      @conf ||= ProcessingFramework::ConfigLoader.new(@@config)
     end
+
+    def self.default_config filename=nil
+      @@config ||= "#{filename}.yml" unless filename.nil?
+      @@config
+    end
+
+    def exit_with_error(message, code)
+      puts "Error: #{message}"
+      exit code
+    end
+
+    def description message
+      @description = message
+    end
+
   end
 end
--- lib/processing_framework/shell_out_helper.rb
+++ lib/processing_framework/shell_out_helper.rb
@@ -1,20 +1,58 @@
+require 'mixlib/shellout'
+
 module ProcessingFramework
   # just a sub, replace with something more useful later..
-  class  ShellOutHelper
-    def ShellOutHelper.run_shell(s, verbose = false)
-      puts("Running: #{s}") if (verbose)
-      start_time = Time.now
-      st = system(s)
-      puts("Done in #{(Time.now - start_time) / 60.0}m.") if (verbose)
-      st
+  module ShellOutHelper
+    SHELL_OUT_DEFAULTS = {live_stream: STDOUT, timeout: 15 * 60}
+
+    def shell_out!(command, opts={})
+      opts = SHELL_OUT_DEFAULTS.merge(opts)
+      cmd = ::Mixlib::ShellOut.new(command, opts)
+      cmd.run_command
+
+      cmd
     end
 
-    def ShellOutHelper.run(s, verbose = false)
-      puts("Running: #{s.join(' ')}") if (verbose)
-      start_time = Time.now
-      st = system(*s)
-      puts("Done in #{(Time.now - start_time) / 60.0}m.") if (verbose)
-      st
+    def inside(directory)
+      begin
+        create_workdir(directory)
+        FileUtils.cd(directory) do
+          yield
+        end
+      rescue RuntimeError => e
+        exit_with_error(e.to_s, 10)
+      ensure
+        cleanup_workdir(directory) unless debug?
+      end
+    end
+
+    def copy_output(output, save_glob = '*', copy_dirs = false)
+      # add trailing slash, if needed
+      output += '/' if output[-1] != '/'
+
+      FileUtils.mkdir_p(output)  unless (File.exist?(output))
+      Dir.glob(save_glob).each do |x|
+        if (File.file?(x) || copy_dirs)
+          puts("INFO: Copying #{x} to #{output}")
+          FileUtils.cp_r(x, output)
+        else
+          puts("INFO: Not a file, skipping #{x} to #{output}")
+        end
+      end
+    end
+
+    private
+    def create_workdir(directory)
+      cleanup_workdir(directory)
+      FileUtils.mkdir_p(directory)
+    end
+
+    def cleanup_workdir(directory)
+      FileUtils.remove_entry_secure(directory) if File.exist?(directory)
+    end
+
+    def debug?
+      true
     end
   end
 end
