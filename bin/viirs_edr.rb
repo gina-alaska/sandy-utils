d5b0da7646b47cfdd815555dd3425ca29b2292f8
Scott Macfarlane <scott@gina.alaska.edu>
2015-08-17 11:40:37 -0800

    First pass at refactoring viirs scripts

--- bin/viirs_edr.rb
+++ bin/viirs_edr.rb
@@ -1,52 +1,30 @@
 #!/usr/bin/env ruby
-# EDR processing tool..
-# Run like:
-# /snpp_edr.rb --inputdir /hub/raid/jcable/sandy/source/npp_test/ -m viirs -p 2 -o /hub/raid/jcable/sandy/output/test_viirs/ -t /hub/raid/jcable/sandy/temp/
-
 ENV['BUNDLE_GEMFILE'] = File.join(File.expand_path('../..', __FILE__), 'Gemfile')
 require 'bundler/setup'
 require 'fileutils'
 require_relative '../lib/processing_framework'
 
 class SnppEDRClamp <  ProcessingFramework::CommandLineHelper
-  @description = 'This tool does EDR processing for SNPP.'
-  @config = ProcessingFramework::ConfigLoader.default_path(__FILE__)
-  @conf = ProcessingFramework::ConfigLoader.new(__FILE__)
+  banner 'This tool does EDR processing for SNPP.'
 
-  option ['-c', '--config'], 'config', "The config file. Using #{@config} as the default.", default: @config
-  option ['-i', '--inputdir'], 'inputdir', 'The input directory. ', required: true
-  option ['-m', '--mode'], 'mode', "The SDR to process, valid options are #{@conf['configs'].keys.join(',')}.", default: 'viirs'
-  option ['-p', '--processors'], 'processors', 'The number of processors to use for processing.',  environment_variable: 'PROCESSING_NUMBER_OF_CPUS', default: @conf['limits']['processor']
+  option ['-p', '--processors'], 'processors', 'The number of processors to use for processing.',  environment_variable: 'PROCESSING_NUMBER_OF_CPUS', default: 1
+
+  parameter "INPUT", 'The input directory'
+  parameter 'OUTPUT', 'The output directory'
 
   def execute
-    conf = ProcessingFramework::ConfigLoader.new(__FILE__)
+    mode = 'default'  # TODO: Refactor yml to not require mode
+    exit_with_error("Unknown/unconfigured mode #{mode}", 19) unless conf['configs'][mode]
+    processing_cfg = conf['configs'][mode]
 
-    output = "#{outdir}"
-    outdir += '/' + basename if basename
     basename = File.basename(inputdir) unless basename
-
-    # check mode
-    fail "Unknown/unconfigured mode #{mode}" unless conf['configs'][mode]
-
     working_dir = "#{tempdir}/#{basename}"
-    begin
-      # make temp space
-      FileUtils.rm_r(working_dir) if File.exist?(working_dir)
-      FileUtils.mkdir(working_dir)
 
-      processing_cfg = conf['configs'][mode]
+    inside(working_dir) do
+      command = ". #{conf['env']} ; #{processing_cfg['driver']} -p #{processors} #{processing_cfg['options']} -i #{input}"
+      shell_out!(command)
 
-      FileUtils.cd(working_dir) do
-        command = ". #{conf['env']} ; #{processing_cfg['driver']} -p #{processors} #{processing_cfg['options']} -i #{inputdir}"
-        fail 'Processing failed.' unless ProcessingFramework::ShellOutHelper.run_shell(command, true)
-        # raise "Processing Failed" if (ProcessingFramework::ShellOutHelper.run_shell
-        copy_output(output)
-      end
-      FileUtils.rm_r(working_dir)
-    rescue RuntimeError => e
-      puts "Error: #{e.to_s}"
-      FileUtils.rm_r(working_dir) if (File.exist?(working_dir))
-      exit(-1)
+      copy_output(output)
     end
   end
 end
--- bin/viirs_geotif.rb
+++ bin/viirs_geotif.rb
@@ -1,63 +1,41 @@
 #!/usr/bin/env ruby
-# Tool to generate geotif stuff for viirs
-#
-# for more info see: viirs_geotif.rb --help
-
 ENV['BUNDLE_GEMFILE'] = File.join(File.expand_path('../..', __FILE__), 'Gemfile')
 require 'bundler/setup'
 require 'fileutils'
 require_relative '../lib/processing_framework'
 
 class ViirsGeotifClamp <  ProcessingFramework::CommandLineHelper
-  @description = 'This tool takes VIIRS data and makes geotifs .'
-  @config = ProcessingFramework::ConfigLoader.default_path(__FILE__)
-  @conf = ProcessingFramework::ConfigLoader.new(__FILE__)
+  banner 'This tool takes VIIRS data and makes geotifs .'
 
-  option ['-c', '--config'], 'config', "The config file. Using #{@config} as the default.", default: @config
-  option ['-i', '--inputdir'], 'inputdir', 'The input directory. ', required: true
-  option ['-m', '--mode'], 'mode', "The mode to use #{@conf['configs'].keys.join(',')}.", default: 'default'
+  option ['-m', '--mode'], 'mode', "The mode to use.", default: 'default'
   option ['-p', '--processors'], 'processors', 'The number of processors to use for processing.',  environment_variable: 'PROCESSING_NUMBER_OF_CPUS', default: @conf['limits']['processor']
 
+  parameter "INPUT", "The input directory"
+  parameter "OUTPUT", "The output directory"
+
   def execute
-    conf = ProcessingFramework::ConfigLoader.new(__FILE__)
+    exit_with_error("Unknown/unconfigured mode #{mode}", 19) unless conf['configs'][mode]
+    processing_cfg = conf['configs'][mode]
 
-    output = "#{outdir}"
-    outdir += '/' + basename if basename
-    basename = File.basename(inputdir) unless basename
-
-    # check mode
-    fail "Unknown/unconfigured mode #{mode}" unless conf['configs'][mode]
-    processing_cfg = conf['configs']["#{mode}"]
-
+    basename = File.basename(input) unless basename
     working_dir = "#{tempdir}/#{basename}"
 
-    begin
-     # make temp space
-     # FileUtils.rm_r(working_dir) if (File.exist?(working_dir))
-     # FileUtils.mkdir(working_dir)
-
-     FileUtils.cd(working_dir) do
-       save_list = []
-       # unless (ProcessingFramework::ShellOutHelper.run_shell(command))
-       generate_300(processing_cfg, inputdir, processors)
-       generate_600(processing_cfg, inputdir, processors)
-       processing_cfg['combinations'].each { |x| save_list += generate_image(x) }
-       processing_cfg['single_bands'].each { |x| save_list += generate_image_sb(x) }
-       save_list += reformat_and_rename_dnb(processing_cfg)
-       copy_output(output, save_list)
-     end
-     FileUtils.rm_r(working_dir)
-   rescue RuntimeError => e
-     puts "Error: #{e.to_s}"
-     FileUtils.rm_r(working_dir) if (File.exist?(working_dir))
-     exit(-1)
-   end
+    inside(working_dir) do
+      save_list = []
+      generate_300(processing_cfg, input, processors)
+      generate_600(processing_cfg, input, processors)
+      processing_cfg['combinations'].each { |x| save_list += generate_image(x) }
+      processing_cfg['single_bands'].each { |x| save_list += generate_image_sb(x) }
+      save_list += reformat_and_rename_dnb(processing_cfg)
+      copy_output(output, save_list)
+    end
   end
 
   # generates i bands
   def generate_300(cfg, i, p)
     command = "#{cfg['polar2grid_driver']} --num-procs #{p} -g #{cfg['igrid']} -d #{i} #{cfg['polar2_grid_options']} --grid-configs #{get_grid_path(cfg)}"
-    fail "ERROR: the command \"#{command}\" failed." unless (ProcessingFramework::ShellOutHelper.run_shell(command))
+    result = shell_out!(command)
+    exit_with_error("#{command} failed.", 12) if result.status != 0
     # save the i bands
     cleanup("*#{cfg['mgrid']}.tif", /npp_viirs_i_\d\d_\w+/)
   end
@@ -65,8 +43,8 @@ class ViirsGeotifClamp <  ProcessingFramework::CommandLineHelper
   # generates m bands
   def generate_600(cfg, i, p)
     command = "#{cfg['polar2grid_driver']} --num-procs #{p} -g #{cfg['mgrid']} -d #{i} #{cfg['polar2_grid_options']} --grid-configs #{get_grid_path(cfg)}"
-    fail "ERROR: the command \"#{command}\" failed." unless (ProcessingFramework::ShellOutHelper.run_shell(command))
-
+    result = shell_out!(command)
+    exit_with_error("#{command} failed.", 12) if results.status != 0
     # save the m bands and the dnb band.
     cleanup("*#{cfg['mgrid']}.tif", /npp_viirs_m_\d\d_\w+|npp_viirs_dnb_\w+/)
   end
@@ -131,21 +109,21 @@ class ViirsGeotifClamp <  ProcessingFramework::CommandLineHelper
     puts tmp_name
 
     # make vrt
-    ProcessingFramework::ShellOutHelper.run_shell("gdalbuildvrt -resolution highest -separate #{tmp_name}.vrt #{red} #{green} #{blue}")
+    shell_out!("gdalbuildvrt -resolution highest -separate #{tmp_name}.vrt #{red} #{green} #{blue}")
     # stretch
-    ProcessingFramework::ShellOutHelper.run_shell("gdal_contrast_stretch #{image_hsh['stretch']} #{tmp_name}.vrt #{tmp_name}.tif")
+    shell_out!("gdal_contrast_stretch #{image_hsh['stretch']} #{tmp_name}.vrt #{tmp_name}.tif")
 
     if (bands['p'])
       pan = get_band(bands['p'])
-      ProcessingFramework::ShellOutHelper.run_shell("gdal_contrast_stretch #{image_hsh['stretch']} #{pan} #{pan}.tmp")
-      ProcessingFramework::ShellOutHelper.run_shell("gdal_landsat_pansharp -ndv 0 -rgb #{tmp_name}.tif -pan #{pan}.tmp -o #{tmp_file}.pan.tif")
-      ProcessingFramework::ShellOutHelper.run_shell("rm -v #{pan}.tmp", opts)
+      shell_out!("gdal_contrast_stretch #{image_hsh['stretch']} #{pan} #{pan}.tmp")
+      shell_out!("gdal_landsat_pansharp -ndv 0 -rgb #{tmp_name}.tif -pan #{pan}.tmp -o #{tmp_file}.pan.tif")
+      shell_out!("rm -v #{pan}.tmp", opts)
       tmp_file =  "#{tmp_file}.pan"
     end
 
     reformat_geotif("#{tmp_name}.tif", "#{final_file}.tif")
-    ProcessingFramework::ShellOutHelper.run_shell("rm -v  #{tmp_name}.vrt #{tmp_name}.tif")
-    ProcessingFramework::ShellOutHelper.run_shell("gdal_translate -of png -outsize 1000 1000 #{final_file}.tif #{final_file}.small.png")
+    shell_out!("rm -v #{tmp_name}.vrt #{tmp_name}.tif")
+    shell_out!("gdal_translate -of png -outsize 1000 1000 #{final_file}.tif #{final_file}.small.png")
 
     ["#{final_file}.tif", "#{final_file}.small.png"]
   end
@@ -167,7 +145,7 @@ class ViirsGeotifClamp <  ProcessingFramework::CommandLineHelper
     final_file = basename
     tmp_name = basename + '.tmp'
 
-    ProcessingFramework::ShellOutHelper.run_shell("gdal_contrast_stretch #{image_hsh['stretch']} #{band} #{tmp_name}.tif")
+    shell_out!("gdal_contrast_stretch #{image_hsh['stretch']} #{band} #{tmp_name}.tif")
     reformat_geotif(tmp_name + '.tif', final_file + '.tif')
     [final_file + '.tif']
   end
@@ -175,8 +153,8 @@ class ViirsGeotifClamp <  ProcessingFramework::CommandLineHelper
   def reformat_geotif(infile, outfile)
     # gdal opts
     gdal_opts = "-co TILED=YES -co COMPRESS=LZW -a_nodata \"0 0 0\" "
-    ProcessingFramework::ShellOutHelper.run_shell("gdal_translate #{gdal_opts} #{infile} #{outfile} ")
-    ProcessingFramework::ShellOutHelper.run_shell("add_overviews.rb #{outfile} ")
+    shell_out!("gdal_translate #{gdal_opts} #{infile} #{outfile} ")
+    shell_out!("add_overviews.rb #{outfile} ")
   end
 
   def copy_output(output, list)
