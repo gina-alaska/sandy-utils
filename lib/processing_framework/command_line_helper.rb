7cad01faf3af60fb6d9f1d3834cf0151996d784d
Scott Macfarlane <scott@gina.alaska.edu>
2015-08-20 09:29:06 -0800

    Cleanup

--- lib/processing_framework/command_line_helper.rb
+++ lib/processing_framework/command_line_helper.rb
@@ -2,19 +2,20 @@ module ProcessingFramework
   require 'clamp'
   require 'fileutils'
   require_relative 'shell_out_helper'
+  require_relative 'compress_helper'
   require_relative 'config_helper'
 
   class CommandLineHelper < Clamp::Command
     include ProcessingFramework::ShellOutHelper
+    include ProcessingFramework::CompressHelper
 
     option ['-c', '--config'], 'config', "Override default config file." do |c|
-      default_config File.realpath(c)
+      @@config = File.realpath(c)
     end
     option ['-b', '--basename'], 'basename', 'The basename of the data to be processed. For example npp.14300.1843 .  Appended to --outputdir if included.'
     option ['-t', '--tempdir'], 'tempdir', 'The temp directory, used for working space. A sub directory will made inside this named after the basename.',  environment_variable: 'PROCESSING_TEMPDIR', required: true
     # option ['-i', '--input'], 'input', 'The input file. ', required: true
     # option ['-o', '--outdir'], 'outdir', 'The output directory. If --basename is included, it is appended to this.', required: true
-
 
     def execute
       fail 'CommandLineHelper should not be instatiated directly.'
@@ -33,10 +34,5 @@ module ProcessingFramework
       puts "Error: #{message}"
       exit code
     end
-
-    def description message
-      @description = message
-    end
-
   end
 end
