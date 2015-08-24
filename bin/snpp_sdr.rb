b01599e21c6a60cf7d68220106faa17eb5da7230
Scott Macfarlane <scott@gina.alaska.edu>
2015-08-24 09:56:13 -0800

    Finish refactor of SNPP SDR

--- bin/snpp_sdr.rb
+++ bin/snpp_sdr.rb
@@ -12,7 +12,7 @@ class SnppViirsSdrClamp <  ProcessingFramework::CommandLineHelper
   default_config 'snpp_sdr'
   banner 'This tool does SDR processing for SNPP.'
 
-  option ['-m', '--mode'], 'mode', "The SDR to process, valid options are #{@conf['configs'].keys.join(',')}.", default: 'viirs'
+  option ['-m', '--mode'], 'mode', "The SDR to process, valid options are viirs, cris, atms.", default: 'viirs'
   option ['-p', '--processors'], 'processors', 'The number of processors to use for processing.',  environment_variable: 'PROCESSING_NUMBER_OF_CPUS', default: 1
 
   parameter "INPUT", "Input directory"
@@ -21,17 +21,22 @@ class SnppViirsSdrClamp <  ProcessingFramework::CommandLineHelper
   def execute
     exit_with_error("Unknown/unconfigured mode: #{mode}", 19) unless conf['configs'][mode]
 
-    basename = File.basename(inputdir) unless basename
+    basename = File.basename(input) unless basename
 
     working_dir = "#{tempdir}/#{basename}"
     inside(working_dir) do
-      processing_cfg = conf['configs']["#{mode}"]
+      processing_cfg = conf['configs'][mode]
 
-        command = ". #{conf['env']} ; #{conf['driver']} -p #{processors} #{conf['options']}  #{input}/#{conf['rdr_glob']}"
-        result = shell_out!(command)
+      input_file = if File.exist?(input) && !File.directory?(input)
+                     input
+                   else
+                     Dir.glob(File.join(input, processing_cfg['rdr_glob'])).first
+                   end
 
-        copy_output(output, '*.h5')
-      end
+      command = ". #{conf['env']} ; #{processing_cfg['driver']} -p #{processors} #{processing_cfg['options']}  #{input_file}"
+      result = shell_out!(command)
+
+      copy_output(output, '*.h5')
     end
   end
 end
