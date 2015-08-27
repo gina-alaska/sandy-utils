3a1cf2ab1b218b4be540638bfd68e2b92ddce8ba
Scott Macfarlane <scott@gina.alaska.edu>
2015-08-27 10:14:16 -0800

    Make viirs_awips work

--- bin/viirs_awips.rb
+++ bin/viirs_awips.rb
@@ -7,7 +7,7 @@ require_relative '../lib/processing_framework'
 class SnppAwipsClamp <  ProcessingFramework::CommandLineHelper
   banner 'This tool takes VIIRS data and makes it AWIPS ready.'
   default_config 'viirs_awips'
-  
+
   option ['-p', '--processors'], 'processors', 'The number of processors to use for processing.',  environment_variable: 'PROCESSING_NUMBER_OF_CPUS', default: 1
 
   parameter "INPUT", "The input directory"
@@ -17,17 +17,17 @@ class SnppAwipsClamp <  ProcessingFramework::CommandLineHelper
     mode = 'default' #TODO:  Refactor yml to not require mode.
     exit_with_error("Unknown/unconfigured mode #{mode}", 19) unless conf['configs'][mode]
 
-    basename = File.basename(inputdir) unless basename
+    basename = File.basename(input) unless basename
     processing_cfg = conf['configs'][mode]
 
     working_dir = "#{tempdir}/#{basename}"
     inside(working_dir) do
-      command = "#{conf['driver']} #{processing_cfg['options']} -g  #{processing_cfg['grid']}  -d #{input} "
-      shell_out!(command)
+      command = "#{conf['driver']} #{processing_cfg['options']} -g #{processing_cfg['grid']} -d #{input} "
+      shell_out!(command, timeout: 9000)
       Dir.glob('SSEC_AWIPS*') do |awips_file|
         gzip!(awips_file)
       end
-      copy_output(output, processing_cfg['save'])
+      copy_output(output, '*')#processing_cfg['save'])
     end
   end
 end
