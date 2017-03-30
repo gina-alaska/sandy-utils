db10f9b4bd0368d9d03bb64de0505da2575f5ee8
JC <jc@alaska.edu>
2017-03-30 14:47:51 -0800

    Added a bunch of date checking.  Hopefully fix issues.
    Signed-off-by: JC <jc@alaska.edu>

--- bin/avhrr_l0.rb
+++ bin/avhrr_l0.rb
@@ -34,21 +34,44 @@ class AVHRRL0Clamp < ProcessingFramework::CommandLineHelper
     end
   end
 
+
+  ##
+  # This checked to see if the data of the processed data is within 10 minutes of the source timestamp.
+  # and is not more than 2 days into the future. 
+  
   def check_date(source_file)
     avhrr_file = Dir.glob('*.avhrr').first
     raise 'No avhrr file found, the processing must have failed' unless avhrr_file
 
-    # time of processed data
-    time_of_data = get_time(avhrr_file)
-    # time of recieved pass
-    time_of_source = get_time(source_file)
 
+    #Catch exception in case of wierd dates, like for example "n19.17366.2352.avhrr"
+    begin
+      # time of processed data
+      time_of_data = get_time(avhrr_file)
+      # time of recieved pass
+      time_of_source = get_time(source_file)
+    rescue ArgumentError => e
+    	puts("ERROR: An error occured parsing the source or processed filenames.")
+	puts("ERROR: \t#{e.to_s}")
+        raise 'Processed data has a strange time'
+    end
+
+    #if the time of the processed data and the source data is different by more than 10 minutes, report as bad
     if ((time_of_data - time_of_source) > 600)
       puts('ERROR: Processed data seems to have a strange time')
       puts("ERROR: Source: #{source_file}, time => #{time_of_source.to_s}")
       puts("ERROR: Data:  #{avhrr_file}, time => #{time_of_data.to_s}")
       raise 'Processed data has a strange time'
     end
+
+    #If the date of the data is more than 2 days in the future, report as bad. 
+    if ((time_of_data - DateTime.now ) > 2*24*60*60)
+      puts('ERROR: Processed data is in the future')
+      puts("ERROR: Source: #{source_file}, time => #{time_of_source.to_s}")
+      puts("ERROR: Data:  #{avhrr_file}, time => #{time_of_data.to_s}")
+      raise 'Processed data has a date in the future'
+    end
+
   end
 
   # returns date of avhrr file.
