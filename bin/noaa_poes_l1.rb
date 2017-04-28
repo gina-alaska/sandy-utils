a1e2cccbb0ba39a0dafb64adb142b883d70c2191
JC <jc@alaska.edu>
2017-04-27 17:10:01 -0800

    Add noaa poes support for aapp.
    Signed-off-by: JC <jc@alaska.edu>

---
+++ bin/noaa_poes_l1.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+# NOAA POES AAPP L1 processing
+#
+
+ENV['BUNDLE_GEMFILE'] = File.join(File.expand_path('../..', __FILE__), 'Gemfile')
+require 'bundler/setup'
+require 'fileutils'
+require_relative '../lib/processing_framework'
+
+class NOAAL0Clamp <  ProcessingFramework::CommandLineHelper
+  default_config 'noaa_l1'
+  banner 'This tool processes raw NOAA POES data to AAPP l1b.'
+
+  parameter 'INPUT', 'The input file or directory'
+  parameter 'OUTPUT', 'The output directory'
+
+  def execute
+    basename = File.basename(input) unless basename
+    @working_dir = "#{tempdir}/#{basename}"
+
+    inside(@working_dir) do
+      get_tle
+      raw_input = strip_header
+      run_aapp_noaa_poes(raw_input, 'AVHRR')
+      run_aapp_noaa_poes(raw_input, 'AMSU-A AMSU-B MHS DCS')
+
+      copy_output(output, '*.l1*')
+    end
+  end
+
+  private
+
+  def exit_with_error(message, code)
+    puts "Error: #{message}"
+    exit code
+  end
+
+  def run_aapp_noaa_poes(input_file, sensors, g = ' ')
+    command = "AAPP_RUN_NOAA -i '#{sensors}' -g '#{g}' -o #{@working_dir} #{input_file}"
+    shell_out(command, env: conf['env'])
+  end
+
+  def strip_header
+    sourcefile = File.basename(input)
+    FileUtils.cp(input, sourcefile)
+    sourcefile = uncompress(sourcefile)
+    shell_out!("dd bs=2176 if=#{sourcefile} of=#{sourcefile}.hrp skip=1")
+    "#{sourcefile}.hrp"
+  end
+
+  #don't fetch a tle each time run
+  def get_tle
+    command = 'get_tle'
+    tle_pattern = Time.now.strftime('/opt/aapp/AAPP/orbelems/tle_db/%Y-%m/tle_%Y%m%d*')
+    tles = Dir.glob(tle_pattern)
+    # fetch tles once per day
+    if (tles.length <= 1)
+      shell_out!(command, env: conf['env'])
+    else
+      puts("INFO: skipping tle fetching, found [#{tles.join(',')}]")
+    end
+  end
+end
+
+NOAAL0Clamp.run
