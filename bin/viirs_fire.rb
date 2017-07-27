16b317a585f6405a8238c761009ecaab606dbd8b
JC <jc@alaska.edu>
2017-07-26 16:10:13 -0800

    Rubocop cleanups, and remove old modis stuff. Still needs tweeks after I double check how the nasa tools show up on nrt vms, right now paths are for my test box.
    Signed-off-by: JC <jc@alaska.edu>

--- bin/viirs_fire.rb
+++ bin/viirs_fire.rb
@@ -27,31 +27,10 @@ class ViirsFireClamp <  ProcessingFramework::CommandLineHelper
     inside(working_dir) do
       processing_cfg = conf['configs'][mode]
 
-      puts build_sdr_run_line(input, processing_cfg)
+      result = shell_out!(build_sdr_run_line(input, processing_cfg))
 
-      #command = "#{processing_cfg['driver']} #{processing_cfg['options']}  #{hdf_1k} #{hdf_geo} #{fire_points}"
-      #result = shell_out!(command)
-
-      #command = "#{processing_cfg['txt_driver']} #{fire_points} > #{fire_points}.txt"
-      #result = shell_out!(command)
-
-      #copy_output(output, processing_cfg['save'])
+      copy_output(output, processing_cfg['save'])
     end
-  end
-
-  def get_output_filename(geo, cfg)
-    get_platform(geo) + '.' + get_modis_l1_time(geo).strftime(cfg['output'])
-  end
-
-  def get_platform(s)
-    puts(s)
-    File.basename(s)[0, 2]
-  end
-
-  def get_modis_l1_time(s)
-    filename = File.basename(s)
-    puts filename[3, 10]
-    DateTime.strptime(filename[3, 10], '%y%j.%H%M')
   end
 
   def copy_output(output, list)
@@ -64,84 +43,83 @@ class ViirsFireClamp <  ProcessingFramework::CommandLineHelper
     end
   end
 
+  # builds sdr run line
   def build_sdr_run_line(input, cfg)
-	command_line =[]
+    command_line = []
 
-	#driver
-	command_line << cfg["driver"]
-	
-	#get the required bands and their input lines
-	command_line << get_input_items(input, cfg)
+    # driver
+    command_line << cfg['driver']
 
-	#get the output txt line
-	command_line << get_output_txt_file(input,cfg)
-	
-	#get the output hdf file line
-	command_line << get_output_hdf_file(input,cfg)
+    # get the required bands and their input lines
+    command_line << get_input_items(input, cfg)
 
-	command_line.join(" ")
+    # get the output txt line
+    command_line << get_output_txt_file(input, cfg)
+
+    # get the output hdf file line
+    command_line << get_output_hdf_file(input, cfg)
+
+    command_line.join(' ')
   end
 
+  # figures out what the output files should be named like
+  def get_output_file_template(input, cfg, data_type, ext)
+    # get sample SDR for date..
+    sample_sdr = get_an_sdr(input, cfg['input'].first)
 
-#figures out what the output files should be named like
-def get_output_file_template(input,cfg,data_type,ext)
-	#get sample SDR for date..
-	sample_sdr = get_an_sdr(input, cfg["input"].first)
-	
-	#strip off first and last sections
-        # as a reminder, format is like so:
-	#SVM16_npp_d20170706_t1941060_e1951040_b00001_c20170725182147627333_gina.h5
-	#  A   B   C         D        E        F      G                     H
-        # A: file_type
-        # B: platform
-        # C: date
-        # D: start time
-  	# E: stop time
-        # F: orbit number
-        # F: date and time the file was created, in YYYMMMDDHHMMSS.SSSSSS format
-	# H: source (we set this to gina)
-	
-	#plan:
- 	#keep B - F, append a new G, and add H
+    # strip off first and last sections
+    # as a reminder, format is like so:
+    # SVM16_npp_d20170706_t1941060_e1951040_b00001_c20170725182147627333_gina.h5
+    #  A   B   C         D        E        F      G                     H
+    # A: file_type
+    # B: platform
+    # C: date
+    # D: start time
+    # E: stop time
+    # F: orbit number
+    # F: date and time the file was created, in YYYMMMDDHHMMSS.SSSSSS format
+    # H: source (we set this to gina)
 
-	new_file_name = [data_type] 
-	new_file_name << File.basename(sample_sdr).split("_")[1,5]
-	new_file_name << Time.now.utc.strftime('c%Y%m%d%H%M%S%6N')
-	#Fixme possibly: perhaps should be a config item
-	new_file_name << "gina"
+    # plan:
+    # keep B - F, append a new G, and add H
 
-	new_file_name.join("_")+"." + ext
-end
+    new_file_name = [data_type]
+    new_file_name << File.basename(sample_sdr).split('_')[1, 5]
+    new_file_name << Time.now.utc.strftime('c%Y%m%d%H%M%S%6N')
+    # Fixme possibly: perhaps should be a config item
+    new_file_name << 'gina'
 
-#gets command line option for text detections file
-def get_output_txt_file(input, cfg)
-	[cfg["output_txt"], get_output_file_template(input,cfg,cfg["output_name"], "txt")].join(" ")
-end
+    new_file_name.join('_') + '.' + ext
+  end
 
-#gets command line option for hdf5 file
-def get_output_hdf_file(input, cfg)
-        [cfg["output_hdf"], get_output_file_template(input,cfg,cfg["output_name"], "h5")].join(" ")
-end
+  # gets command line option for text detections file
+  def get_output_txt_file(input, cfg)
+    [cfg['output_txt'], get_output_file_template(input, cfg, cfg['output_name'], 'txt')].join(' ')
+  end
 
-  #builds up the viirs.svdjlkf /path info required to run the SPAs
+  # gets command line option for hdf5 file
+  def get_output_hdf_file(input, cfg)
+    [cfg['output_hdf'], get_output_file_template(input, cfg, cfg['output_name'], 'h5')].join(' ')
+  end
+
+  # builds up the viirs.svdjlkf /path info required to run the SPAs
   def get_input_items(input, cfg)
-	input_items = []
-	cfg["input"].each do |item|
-		input_items << item
-		input_items << get_an_sdr(input, item)	
-	end
-	
-	input_items.join(" ")
+    input_items = []
+    cfg['input'].each do |item|
+      input_items << item
+      input_items << get_an_sdr(input, item)
+    end
+
+    input_items.join(' ')
   end
 
-  #fetchs the sdr for the particular type - for example "viirs.svm09"
+  # fetchs the sdr for the particular type - for example "viirs.svm09"
   def get_an_sdr(input, sdr_type)
-	band = sdr_type.split(".")[1]
-	bands = Dir.glob(input + "/" + band.upcase + "*.h5")
-	raise "Could not find SDR for #{sdr_type} in #{input}" if bands.length != 1
-	bands.first
+    band = sdr_type.split('.')[1]
+    bands = Dir.glob(input + '/' + band.upcase + '*.h5')
+    fail "Could not find SDR for #{sdr_type} in #{input}" if bands.length != 1
+    bands.first
   end
-
 end
 
 ViirsFireClamp.run
