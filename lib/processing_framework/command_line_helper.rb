02ca192830ff67620177b63b53fec69bf9d0e247
JC <jay@alaska.edu>
2018-05-31 10:46:37 -0800

    Added in a tweek to say what node name/host name the job is run on, for trouble shooting.
    Signed-off-by: JC <jay@alaska.edu>

--- lib/processing_framework/command_line_helper.rb
+++ lib/processing_framework/command_line_helper.rb
@@ -1,6 +1,7 @@
 module ProcessingFramework
   require 'clamp'
   require 'fileutils'
+  require 'socket'
   require_relative 'shell_out_helper'
   require_relative 'compress_helper'
   require_relative 'config_helper'
@@ -9,23 +10,28 @@ module ProcessingFramework
     include ProcessingFramework::ShellOutHelper
     include ProcessingFramework::CompressHelper
 
-    option ['-c', '--config'], 'config', "Override default config file." do |c|
+    option ['-c', '--config'], 'config', 'Override default config file.' do |c|
       @@config = File.realpath(c)
     end
     option ['-b', '--basename'], 'basename', 'The basename of the data to be processed. For example npp.14300.1843 .  Appended to --outputdir if included.'
-    option ['-t', '--tempdir'], 'tempdir', 'The temp directory, used for working space. A sub directory will made inside this named after the basename.',  environment_variable: 'PROCESSING_TEMPDIR', required: true
+    option ['-t', '--tempdir'], 'tempdir', 'The temp directory, used for working space. A sub directory will made inside this named after the basename.', environment_variable: 'PROCESSING_TEMPDIR', required: true
     # option ['-i', '--input'], 'input', 'The input file. ', required: true
     # option ['-o', '--outdir'], 'outdir', 'The output directory. If --basename is included, it is appended to this.', required: true
 
+    def initialize(one, two)
+      super(one, two)
+      puts("INFO: Running on #{Socket.gethostname}")
+    end
+
     def execute
-      fail 'CommandLineHelper should not be instatiated directly.'
+      raise 'CommandLineHelper should not be instatiated directly.'
     end
 
     def conf
       @conf ||= ProcessingFramework::ConfigLoader.new(@@config)
     end
 
-    def self.default_config filename=nil
+    def self.default_config(filename = nil)
       @@config ||= "#{filename}.yml" unless filename.nil?
       @@config
     end
