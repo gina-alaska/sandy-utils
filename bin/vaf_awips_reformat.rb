ed51b19bac1242dc1af98ba02ea403c289fcbc0b
JC <jay@alaska.edu>
2022-04-29 16:52:57 +0000

    Changes to accomidate the way the python tool works.
    Signed-off-by: JC <jay@alaska.edu>

--- bin/vaf_awips_reformat.rb
+++ bin/vaf_awips_reformat.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 # Run like:
-#vaf_awips_reformat.rb -t temp in out
+# vaf_awips_reformat.rb -t temp in out
 
-ENV['BUNDLE_GEMFILE'] = File.join(File.expand_path('../..', __FILE__), 'Gemfile')
+ENV['BUNDLE_GEMFILE'] = File.join(File.expand_path('..', __dir__), 'Gemfile')
 require 'bundler/setup'
 require 'fileutils'
 require_relative '../lib/processing_framework'
 
-class Vaf4AwipsClamp <  ProcessingFramework::CommandLineHelper
+class Vaf4AwipsClamp < ProcessingFramework::CommandLineHelper
   banner 'This tool converts heaps '
   default_config 'vaf_awips_reformat'
 
@@ -19,20 +19,21 @@ class Vaf4AwipsClamp <  ProcessingFramework::CommandLineHelper
   def execute
     exit_with_error("Unknown/unconfigured mode: #{mode}", 19) unless conf['configs'][mode]
 
-    basename = File.basename(input) unless basename
+    basename ||= File.basename(input)
 
     working_dir = "#{tempdir}/#{basename}"
     inside(working_dir) do
       processing_cfg = conf['configs'][mode]
       processing_cfg['to_process_glob'].each do |glob|
-	Dir.glob(input+"/"+glob).each do |item|
-		command = "#{processing_cfg['driver']} #{processing_cfg['options']} -f #{item}"
-      		result = shell_out(command, clean_environment: true)
-      		copy_output(output, processing_cfg['save'])
-	end
+        Dir.glob(input + '/' + glob).each do |item|
+          FileUtils.cp(item, '.')
+          command = "#{processing_cfg['driver']} #{processing_cfg['options']} -f #{File.basename(item)}"
+          result = shell_out(command, clean_environment: true)
+        end
+      end
+      processing_cfg['save'].each do |pattern|
+        copy_output(output, pattern)
       end
     end
   end
 end
-
-Vaf4AwipsClamp.run
