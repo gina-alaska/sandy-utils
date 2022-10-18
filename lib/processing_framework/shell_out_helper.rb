b2060922c33ef0d73d423191f998848286b82436
JC <spruceboy@gmail.com>
2022-10-18 08:06:10 -0800

    Added s3 push. (#118)

--- lib/processing_framework/shell_out_helper.rb
+++ lib/processing_framework/shell_out_helper.rb
@@ -1,5 +1,6 @@
 require 'mixlib/shellout'
 require 'shellwords'
+require "aws-sdk-s3"
 
 module ProcessingFramework
   module ShellOutHelper
@@ -45,15 +46,34 @@ module ProcessingFramework
       cleanup_workdir(directory) unless debug?
     end
 
+
     def copy_output(output, save_glob = '*', copy_dirs = false)
+      # check to see if output dir is a s3 url..
+      s3 = false
+      if output =~ %r{^s3://.*} 
+	     # maybe should support this format as well
+	     #|| output =~ /^arn:aws:s3\.*/
+             s3= /^s3:\/\/([a-zA-Z0-9\-\.]+)/.match(output)[1]
+      end
+
       # add trailing slash, if needed
       output += '/' if output[-1] != '/'
 
-      FileUtils.mkdir_p(output) unless File.exist?(output)
+      FileUtils.mkdir_p(output) unless !s3 && File.exist?(output)
       Dir.glob(save_glob).each do |x|
         if (File.file?(x) || copy_dirs)
-          puts("INFO: Copying #{x} to #{output}")
-          FileUtils.cp_r(x, output)
+          if s3
+            begin
+              s3_object = Aws::S3::Object.new(s3, File.basename(x))
+              s3_object.upload_file(x)
+            rescue Aws::Errors::ServiceError => e
+              puts "ERROR: Couldn't upload file #{file_path} to #{s3_object.key}. Here's why: #{e.message}"
+              exit_with_error(e.to_s, 10)
+            end
+          else
+            puts("INFO: Copying #{x} to #{output}")
+            FileUtils.cp_r(x, output)
+          end
         else
           puts("INFO: Not a file, skipping #{x} to #{output}")
         end
