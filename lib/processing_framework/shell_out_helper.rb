8a25070da50a6a4e1e3a6c1f73bf16665ccd7042
JC <spruceboy@gmail.com>
2022-10-24 15:12:54 -0800

    Added subdirecttories into s3, hopefully (#120)

--- lib/processing_framework/shell_out_helper.rb
+++ lib/processing_framework/shell_out_helper.rb
@@ -1,6 +1,6 @@
 require 'mixlib/shellout'
 require 'shellwords'
-require "aws-sdk-s3"
+require 'aws-sdk-s3'
 
 module ProcessingFramework
   module ShellOutHelper
@@ -46,14 +46,20 @@ module ProcessingFramework
       cleanup_workdir(directory) unless debug?
     end
 
-
     def copy_output(output, save_glob = '*', copy_dirs = false)
       # check to see if output dir is a s3 url..
       s3 = false
-      if output =~ %r{^s3://.*} 
-	     # maybe should support this format as well
-	     #|| output =~ /^arn:aws:s3\.*/
-             s3= /^s3:\/\/([a-zA-Z0-9\-\.]+)/.match(output)[1]
+      s3_prefix = ''
+      if output =~ %r{^s3://.*}
+        # maybe should support this format as well
+        # || output =~ /^arn:aws:s3\.*/
+        s3, s3_prefix = %r{^s3://([a-zA-Z0-9\-.]+)([/a-zA-Z0-9\-.]*)}.match(output)[1, 2]
+        if !s3_prefix || s3_prefix == '' || s3_prefix == '/' # catch odd values, make sure they are empty
+          s3_prefix = ''
+        elsif s3_prefix[-1] != '/'
+          s3_prefix += '/'
+        end
+        s3_prefix.delete_prefix!('/')
       end
 
       # add trailing slash, if needed
@@ -64,15 +70,21 @@ module ProcessingFramework
         if (File.file?(x) || copy_dirs)
           if s3
             begin
-              s3_object = Aws::S3::Object.new(s3, File.basename(x))
+              s3_object = Aws::S3::Object.new(
+                s3, s3_prefix += File.basename(x)
+              )
               s3_object.upload_file(x)
             rescue Aws::Errors::ServiceError => e
               puts "ERROR: Couldn't upload file #{file_path} to #{s3_object.key}. Here's why: #{e.message}"
-              exit_with_error(e.to_s, 10)
+              exit_with_error(
+                e.to_s, 10
+              )
             end
           else
             puts("INFO: Copying #{x} to #{output}")
-            FileUtils.cp_r(x, output)
+            FileUtils.cp_r(
+              x, output
+            )
           end
         else
           puts("INFO: Not a file, skipping #{x} to #{output}")
