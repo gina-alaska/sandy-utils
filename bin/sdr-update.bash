9dc293b99a1576b5c616719176a05d78d2ab12f0
JC <spruceboy@gmail.com>
2024-01-25 10:48:11 -0900

    Update sdr-update.bash
    Make logging a bit better.

--- bin/sdr-update.bash
+++ bin/sdr-update.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -l
 cd /tmp
 source /opt/cspp/SDR_4_0/cspp_sdr_env.sh
-sdr_luts.sh >> $HOME/luts-update.log
-sdr_ancillary.sh >> $HOME/anc-update.log
+sdr_luts.sh 2>&1 >> $HOME/luts-update.log
+sdr_ancillary.sh 2>&1 >> $HOME/anc-update.log
