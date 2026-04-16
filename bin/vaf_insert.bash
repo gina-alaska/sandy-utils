a7ee26dd9018a552415e560075e184ce86855ba7
Jay C <jay@alaska.edu>
2026-04-16 16:24:39 +0000

    Drive script to ingect data..

---
+++ bin/vaf_insert.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#get path to script, hacky
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_PATH="$(dirname "$SCRIPT_PATH")"
+
+#set env
+source "$SCRIPT_PATH"/../env.sh
+
+#set aws credentials
+. /opt/gina/config/aws.env
+
+#inject all txt files
+for vaf_file in $1/*.txt; do
+    if [ -f "$vaf_file" ]; then
+        echo python "$SCRIPT_PATH"/fire_insert.py "$vaf_file"
+    fi
+done
